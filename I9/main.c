794306e22db0eb19ec88e7444ee0b4eff2f6de84
coralia <coralia.glisca@gmail.com>
2017-10-24 15:33:25 -0700

    teme2

--- I9/main.c
+++ I9/main.c
@@ -11,21 +11,21 @@ int main()
     puts("Enter the name of the sports team: "); scanf("%s", &x);
 
     if(x==PIRATES)
-
+    {
         puts("Home run!\n");
-
+    }
     else if (x==STEELERS)
-
+    {
         puts("Touchdown!\n");
-
+    }
     else if (x==PENGUINS)
-
+    {
         puts("Goal!\n");
-
+    }
     else
-
+    {
         puts("You lose!\n");
-
+    }
 
     return 0;
 }
