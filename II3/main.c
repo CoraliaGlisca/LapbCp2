92692a835874379ced31d944667616bf92fda3c1
CoraliaGlisca <32877341+CoraliaGlisca@users.noreply.github.com>
2017-10-30 11:31:00 -0700

    Update main.c

--- II3/main.c
+++ II3/main.c
@@ -12,7 +12,9 @@ int main()
 
     printf("Enter the coefficients a, b and c: ");
 
-    scanf("%f", "%f", "%f", &a, &b, &c);
+    scanf("%f", &a);
+    scanf("%f", &b);
+    scanf("%f", &c);
 
     D = b*b - 4*a*c;
 
